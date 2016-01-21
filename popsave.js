3dbe8ec777e58a5b97198d8ce6833c374317163a
ulyfm <ulyssesfm@gmail.com>
2016-01-21 12:24:31 -0800

    Update popsave.js

--- popsave.js
+++ popsave.js
@@ -3,6 +3,7 @@ function save_options() {
   var mac = document.getElementById('mac').value;
   var mip = document.getElementById('mip').value;
   var map = document.getElementById('map').value;
+  var bip = document.getElementById('bip').value;
   var bypass = document.getElementById('bpf').checked;
   var selfczechs = document.getElementById('sce').checked;
   var enabled = document.getElementById('enable').checked;
@@ -11,6 +12,7 @@ function save_options() {
 	mac: mac,
 	mip: mip,
 	map: map,
+	bip: bip,
     sce: selfczechs,
 	bpf: bypass,
 	ena: enabled
@@ -28,15 +30,17 @@ function restore_options() {
 	mac: 18,
 	mip: 1,
 	map: 20,
+	bip: "10.7.1.210",
     sce: false,
 	bpf: false,
 	ena: true
   }, function(items) {
-    document.getElementById('mic').value = items.mic;
+	document.getElementById('mic').value = items.mic;
 	document.getElementById('mac').value = items.mac;
 	document.getElementById('mip').value = items.mip;
 	document.getElementById('map').value = items.map;
-    document.getElementById('sce').checked = items.sce;
+	document.getElementById('bip').value = items.bip;
+	document.getElementById('sce').checked = items.sce;
 	document.getElementById('bpf').checked = items.bpf;
 	document.getElementById('enable').checked = items.ena;
   });
