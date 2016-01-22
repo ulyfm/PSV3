38c304f8060e5b780afbd4dd5890d33a369ccb3b
ulyfm <ulyssesfm@gmail.com>
2016-01-21 20:13:10 -0800

    fixed some things
    streamlined everything, changed some code

--- scripts/popsave.js
+++ scripts/popsave.js
@@ -1,21 +1,9 @@
 function save_options() {
-  var mic = document.getElementById('mic').value;
-  var mac = document.getElementById('mac').value;
-  var mip = document.getElementById('mip').value;
-  var map = document.getElementById('map').value;
   var bip = document.getElementById('bip').value;
   var bypass = document.getElementById('bpf').checked;
-  var selfczechs = document.getElementById('sce').checked;
-  var enabled = document.getElementById('enable').checked;
   chrome.storage.sync.set({
-    mic: mic,
-	mac: mac,
-	mip: mip,
-	map: map,
-	bip: bip,
-    sce: selfczechs,
-	bpf: bypass,
-	ena: enabled
+    bip: bip,
+    bpf: bypass
   }, function() {
     var status = document.getElementById('status');
     status.textContent = 'saved.';
@@ -26,23 +14,11 @@ function save_options() {
 }
 function restore_options() {
   chrome.storage.sync.get({
-    mic: 1,
-	mac: 18,
-	mip: 1,
-	map: 20,
-	bip: "10.7.1.210",
-    sce: false,
-	bpf: false,
-	ena: true
+    bip: "10.7.1.210",
+    bpf: false
   }, function(items) {
-	document.getElementById('mic').value = items.mic;
-	document.getElementById('mac').value = items.mac;
-	document.getElementById('mip').value = items.mip;
-	document.getElementById('map').value = items.map;
 	document.getElementById('bip').value = items.bip;
-	document.getElementById('sce').checked = items.sce;
 	document.getElementById('bpf').checked = items.bpf;
-	document.getElementById('enable').checked = items.ena;
   });
 }
 document.addEventListener('DOMContentLoaded', restore_options);
